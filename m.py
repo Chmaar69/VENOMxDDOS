afab3b919982729cf9aff3a6f649dc7abbcf56a7
Chmaar69 <husainkhan2709@gmail.com>
2024-06-26 22:00:10 +0530

    Update m.py

--- m.py
+++ m.py
@@ -8,10 +8,10 @@ import os
 from keep_alive import keep_alive
 keep_alive()
 # insert your Telegram bot token here
-bot = telebot.TeleBot('TOKEN')
+bot = telebot.TeleBot('6754892162:AAFB-rjeWB4xDly1gzKF-y60W3h4YKKwMbg')
 
 # Admin user IDs
-admin_id = ["Your Id"]
+admin_id = ["1326970713"]
 
 # File to store allowed user IDs
 USER_FILE = "users.txt"
@@ -24,6 +24,7 @@ def read_users():
     try:
         with open(USER_FILE, "r") as file:
             return file.read().splitlines()
+            
     except FileNotFoundError:
         return []
 
